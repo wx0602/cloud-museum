d95004d662e8018f90ef58cc59f504addad63515
闲乘月 <895377486@qq.com>
2025-12-04 21:28:13 +0800

    修复跳转错误
    Add scrollBehavior to router for improved navigation.

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,12 +27,18 @@ const routes = [
     name: 'Quiz',
     component: () => import('@/views/Quiz.vue'),
   },
-  
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    return { top: 0, left: 0 }
+  }
 })
 
 export default router
