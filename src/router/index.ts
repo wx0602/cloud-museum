751b0d48a5ffbf428393d2337f7f4f4392698a62
闲乘月 <895377486@qq.com>
2025-12-04 21:37:30 +0800

    bug修复
    Removed custom scroll behavior from router configuration.

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,18 +27,12 @@ const routes = [
     name: 'Quiz',
     component: () => import('@/views/Quiz.vue'),
   },
+  
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    }
-    return { top: 0, left: 0 }
-  }
 })
 
 export default router
