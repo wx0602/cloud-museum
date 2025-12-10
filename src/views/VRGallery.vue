ce8f97af7f48f538ffb8c3a1f177b77ddcd0ae18
semiconductor007 <2160422011@qq.com>
2025-12-10 23:16:43 +0800

    进行了元素的添加

--- src/views/VRGallery.vue
+++ src/views/VRGallery.vue
@@ -65,7 +65,6 @@ const vrList = ref([
   {
     id: 1,
     name: "故宫博物院",
-    cover: "https://example.com/img1.jpg",
     description: "推开紫禁城的朱门，你踩响的是六百年华夏文明的回音。",
     tags: ["艺术", "中国"],
     link: "https://www.dpm.org.cn/vr/index.html",
@@ -73,7 +72,6 @@ const vrList = ref([
   {
     id: 2,
     name: "上海博物馆",
-    cover: "https://example.com/img1.jpg",
     description: "当青铜器上的饕餮凝视着你，华夏先祖的智慧正穿透三千年迷雾。",
     tags: ["艺术", "中国"],
     link: "https://www.shanghaimuseum.net/yun/static/1937312374432075776/spg.html?m=1937312374432075776",
@@ -81,7 +79,6 @@ const vrList = ref([
   {
     id: 3,
     name: "中国国家博物馆VR展厅集成",
-    cover: "https://m.chnmuseum.cn/Portals/0/web/vr/",
     description: "从商周甲骨到唐宋文书，这里收藏着汉字跳动的五千年心脏。",
     tags: ["艺术", "中国", "现代艺术", "国际", "历史"],
     link: "https://m.chnmuseum.cn/Portals/0/web/vr/",
@@ -89,7 +86,6 @@ const vrList = ref([
   {
     id: 4,
     name: "上海博物馆（小藏品集成）",
-    cover: "https://www.shanghaimuseum.net/mu/frontend/pg/infomation/ar",
     description: "芥子纳须弥，掌上观乾坤。通过专属工具，细节性触摸大历史中的微小精华。",
     tags: ["艺术", "中国", "历史"],
     link: "https://www.shanghaimuseum.net/mu/frontend/pg/infomation/ar",
@@ -97,16 +93,13 @@ const vrList = ref([
   {
     id: 5,
     name: "北京鲁迅博物馆-主馆",
-    cover: "https://example.com/img3.jpg",
-    description:
-      "走进绍兴文人的静默院落，在墨香中追寻那个年代的呐喊与彷徨。",
+    description:"走进绍兴文人的静默院落，在墨香中追寻那个年代的呐喊与彷徨。",
     tags: ["历史", "中国"],
     link: "https://vr.realsee.cn/cvr/vmldAgGRjjHLWEwt2m/0YJyOo0xSghrg2bdqp4b7FATdhDhrLae/?payload=eyJjbHVzdGVyX2idjozLCJyZWdpb25faWQiOjcsInBhY2thZ2VfaWQiOjcsImN1c3RvbV9pZCI6MjE3NzQ4NjI4MDF9&withGoBack=1&loading=1&ts=1764078330980",
   },
   {
     id: 6,
     name: "上海博物馆(全景集合)",
-    cover: "https://m.chnmuseum.cn/Portals/0/web/vr/",
     description: "海派文化的博大精深。在云端漫步，欣赏典雅精致的江南艺术收藏。",
     tags: ["艺术", "中国", "现代艺术", "国际", "历史"],
     link: "https://www.shanghaimuseum.net/mu/frontend/pg/infomation/three-dimensions?type=permanent",
@@ -114,7 +107,6 @@ const vrList = ref([
   {
     id: 7,
     name: "卢浮宫",
-    cover: "https://example.com/img1.jpg",
     description: "塞纳河畔的艺术王宫，米洛的维纳斯在此静默，达芬奇的微笑永恒。",
     tags: ["艺术", "历史"],
     link: "https://www.louvre.fr/visites-en-ligne/petitegalerie/saison6/",
@@ -122,7 +114,6 @@ const vrList = ref([
   {
     id: 8,
     name: "罗马梵蒂冈博物馆",
-    cover: "https://example.com/img1.jpg",
     description: "仰望西斯廷教堂穹顶，米开朗基罗笔下的人类创世在此定格。",
     tags: ["艺术", "历史"],
     link: "https://www.museivaticani.va/content/museivaticani/en/collezioni/musei/tour-virtuali-elenco.1.html",
@@ -130,20 +121,72 @@ const vrList = ref([
   {
     id: 9,
     name: "美国国立自然历史博物馆",
-    cover: "https://example.com/img1.jpg",
-    description:
-      "从巨型恐龙的骸骨到宇宙星尘，探寻地球与生命亿万年的壮阔进化史。",
+    description:"从巨型恐龙的骸骨到宇宙星尘，探寻地球与生命亿万年的壮阔进化史。",
     tags: ["艺术", "历史"],
     link: "https://naturalhistory2.si.edu/vt3/NMNH/?startscene=21&startactions=lookat(-32.02,2.68,120,0,0);",
   },
   {
     id: 10,
     name: "圣彼得堡冬宫博物馆",
-    cover: "https://example.com/img1.jpg",
-    description:
-      "俄罗斯沙皇的冬日宫殿，用巴洛克的极致奢华承载欧洲艺术的万千珍藏。",
+    description:"俄罗斯沙皇的冬日宫殿，用巴洛克的极致奢华承载欧洲艺术的万千珍藏。",
     tags: ["艺术", "历史"],
     link: "https://pano.hermitagemuseum.org/newpano/ru/main/#node342",
+  },
+  {
+    id: 11,
+    name: "河南博物馆",
+    description:"荟萃中原八千年文明瑰宝。商周青铜、汉唐陶俑，见证华夏起源与兴衰，是触摸中华文脉的殿堂。",
+    tags: ["历史", "中国"],
+    link: "https://www.chnmus.net/ch/exhibitions/virtual/index.html",
+  },
+  {
+    id: 12,
+    name: "良渚博物馆",
+    description:"玉琮神徽、古城水利，展现远古王国智慧，是中华文明探源的核心地标。",
+    tags: ["古历史", "中国"],
+    link: "https://www.lzmuseum.cn/vr/index.html?scene_id=23692226",
+  },
+  {
+    id: 13,
+    name: "海昏侯博物馆",
+    description:"惊世黄金、万枚简牍，揭秘废帝刘贺的传奇生平，是解码西汉盛世的独特钥匙。",
+    tags: ["历史", "中国"],
+    link: "https://www.jxmuseum.cn/xnbz/html/xnbz.html",
+  },
+  {
+    id: 14,
+    name: "苏州博物馆",
+    description:"现代建筑与古典园林的完美交融。吴门书画、玲珑珍玩，诠释江南美学的诗意与精致，是一座会呼吸的艺术殿堂。",
+    tags: ["艺术", "历史"],
+    link: "https://www.szmuseum.com/0pano/new/pano/index.html",
+  },
+  {
+    id: 15,
+    name: "南京博物馆",
+    description:"六朝金粉、明清书画，南北文明在此交融，是纵览华夏万年历史的完整坐标。",
+    tags: ["历史","中国"],
+    link: "https://www.4dkankan.com/spg.html?m=KJ-MaJvGglMJVz&lang=zh",
+  },
+  {
+    id: 16,
+    name: "云南省博物馆",
+    description:"滇国青铜、南诏大理，多元文明与自然生态交融，是领略彩云之南瑰丽神奇的文化窗口。",
+    tags: ["艺术", "历史","中国"],
+    link: "https://www.ynmuseum.org/wszl/",
+  },
+  {
+    id: 17,
+    name: "丝绸之路博物馆",
+    description:"驼铃商队、珍宝荟萃，见证东西方商品、艺术与信仰的融合之路，一座跨越时空的世界之窗。",
+    tags: ["历史","艺术", "国际","中国"],
+    link: "https://www.chnmuseum.cn/portals/0/web/vr/sczl/mobile/",
+  },
+  {
+    id: 18,
+    name: "湖南省博物馆·战国春秋特展",
+    description:"青铜剑戈、简牍思想，七雄并立与文化交融，铺就了华夏一统的深沉基底。",
+    tags: ["古历史","中国"],
+    link: "https://www.hnmuseum.com/sites/default/files/activity/3d/",
   },
 ]);
 
